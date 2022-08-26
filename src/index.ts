6809937a6eb264006e967c24749315b184f51dfc
Michele Riva <ciao@micheleriva.it>
2022-08-26 12:06:02 +0200

    style: fixes linting

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,65 @@
 import type { PropertiesSchema, Lyra } from '@lyrasearch/lyra'
 import { tokenize } from '@lyrasearch/lyra'
 
-export function countOccurrencies<T>(list: T[], target: T): number {
-  return list.reduce((acc, item) => (item === target ? acc + 1 : acc), 0);
+export function countOccurrencies<T> (list: T[], target: T): number {
+  return list.reduce((acc, item) => (item === target ? acc + 1 : acc), 0)
 }
 
-export function getAllTokensInAllDocsByProperty<T extends PropertiesSchema>(
-  lyra: Lyra<T>,
+export function getAllTokensInAllDocsByProperty<T extends PropertiesSchema> (
+  lyra: Lyra<T>
 ): { [key: string]: string[] } {
-  const docs = lyra.docs;
-  const properties = Object.keys(lyra.schema).filter(property => lyra.schema[property] === "string");
-  const individualTokens = properties.reduce((acc, property) => ({ ...acc, [property]: [] }), {});
+  const docs = lyra.docs
+  const properties = Object.keys(lyra.schema).filter(property => lyra.schema[property] === 'string')
+  const individualTokens = properties.reduce((acc, property) => ({ ...acc, [property]: [] }), {})
 
   for (const property of properties) {
-    const individualProperty = (individualTokens as any)[property];
+    const individualProperty = (individualTokens as any)[property]
     for (const doc in docs) {
-      const currentDoc = docs[doc]![property];
-      const tokens = tokenize(currentDoc as string);
-      individualProperty.push(...tokens);
+      const currentDoc = docs[doc]![property]
+      const tokens = tokenize(currentDoc as string)
+      individualProperty.push(...tokens)
     }
 
-    (individualTokens as any)[property] = individualProperty;
+    (individualTokens as any)[property] = individualProperty
   }
 
-  return individualTokens;
+  return individualTokens
 }
 
-export function generateWeights<T extends PropertiesSchema>(lyra: Lyra<T>) {
-  const docs = lyra.docs;
-  const N = Object.keys(docs).length;
-  const allTokensInDocs = getAllTokensInAllDocsByProperty(lyra);
-  const weights = {};
+export function generateWeights<T extends PropertiesSchema> (lyra: Lyra<T>) {
+  const docs = lyra.docs
+  const N = Object.keys(docs).length
+  const allTokensInDocs = getAllTokensInAllDocsByProperty(lyra)
+  const weights = {}
 
   for (const docID in docs) {
-    const properties = docs[docID];
+    const properties = docs[docID]
 
     for (const property in properties) {
       if (!(property in weights)) {
-        // @ts-ignore
-        weights[property] = {};
+        // @ts-expect-error
+        weights[property] = {}
       }
 
-      const d = properties[property];
+      const d = properties[property]
 
-      const tokens = tokenize(d as string, lyra.defaultLanguage, true);
+      const tokens = tokenize(d as string, lyra.defaultLanguage, true)
 
       for (const t of tokens) {
-        const tf = countOccurrencies(tokens, t) / tokens.length;
-        const df = countOccurrencies(allTokensInDocs[property], t);
-        const idf = Math.log(N / df);
-        const tfIdf = tf * idf;
+        const tf = countOccurrencies(tokens, t) / tokens.length
+        const df = countOccurrencies(allTokensInDocs[property], t)
+        const idf = Math.log(N / df)
+        const tfIdf = tf * idf
 
-        // @ts-ignore
+        // @ts-expect-error
         if (!(docID in weights[property])) {
-          // @ts-ignore
+          // @ts-expect-error
           weights[property][docID] = {
-            [t]: tfIdf,
-          };
+            [t]: tfIdf
+          }
         } else {
-          // @ts-ignore
-          weights[property][docID][t] = tfIdf;
+          // @ts-expect-error
+          weights[property][docID][t] = tfIdf
         }
       }
     }
--- tap-snapshots/src/tests/index.test.ts.test.cjs
+++ tap-snapshots/src/tests/index.test.ts.test.cjs
@@ -5,11 +5,11 @@
  * Make sure to inspect the output below.  Do not ignore changes!
  */
 'use strict'
-exports[`src/tests/index.test.ts TAP countOccurrencies it should correctly generate weights > it should correctly generate weights-O1 1`] = `
+exports['src/tests/index.test.ts TAP countOccurrencies it should correctly generate weights > it should correctly generate weights-O1 1'] = `
 undefined
 `
 
-exports[`src/tests/index.test.ts TAP countOccurrencies should correctly get all tokens in all docs by property > should correctly get all tokens in all docs by property-O1 1`] = `
+exports['src/tests/index.test.ts TAP countOccurrencies should correctly get all tokens in all docs by property > should correctly get all tokens in all docs by property-O1 1'] = `
 Object {
   "description": Array [
     "my",
